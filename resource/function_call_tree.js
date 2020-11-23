c09088132297c617c1f1f410bfe4e873ee354145
Wei Zheng <wade.zheng.pro@aliyun.com>
2020-11-24 09:09:45 +1100

    Get ready for fetch the real data

--- resource/function_call_tree.js
+++ resource/function_call_tree.js
@@ -3,19 +3,24 @@ import * as d3 from "d3";
 
 export class FunctionCallTree {
 
-    constructor(callTree) {
+    constructor() {
+    }
+
+    render(callTree) {
+        d3.select("#call-graph-plot").selectAll("*").remove();
+
         const radius = 954 / 2;
 
         const data = d3.hierarchy(callTree)
             .sort((a, b) => d3.ascending(a.data.name, b.data.name));
-    
+
         const tree = d3.tree()
             .size([2 * Math.PI, radius])
             .separation((a, b) => (a.parent === b.parent ? 1 : 2) / a.depth);
-    
+
         const root = tree(data);
         const svg = d3.select("#call-graph-plot").append("svg:svg");
-    
+
         svg.append("g")
             .attr("fill", "none")
             .attr("stroke", "#555")
@@ -27,7 +32,7 @@ export class FunctionCallTree {
             .attr("d", d3.linkRadial()
                 .angle(d => d.x)
                 .radius(d => d.y));
-    
+
         svg.append("g")
             .selectAll("circle")
             .data(root.descendants())
@@ -38,7 +43,7 @@ export class FunctionCallTree {
             `)
             .attr("fill", d => d.children ? "#555" : "#999")
             .attr("r", 2.5);
-    
+
         const textGroup = svg.append("g")
             .attr("font-family", "sans-serif")
             .attr("font-size", 10)
@@ -58,7 +63,7 @@ export class FunctionCallTree {
             .text(d => d.data.name)
             .clone(true).lower()
             .attr("stroke", "white");
-    
+
         const {x, y, width, height} = svg.node().getBBox();
         svg.attr("viewBox", [x, y, width, height]);
     }
--- resource/index.js
+++ resource/index.js
@@ -6,17 +6,14 @@ import {FunctionCallTree} from "./function_call_tree";
     const qWebChannel = require("qwebchannel");
     qWebChannel.QWebChannel(qt.webChannelTransport, function (channel) {
         const signalHub = channel.objects.signalHub;
+        const funcCallTree = new FunctionCallTree();
 
         signalHub.funcCallersTreeGet.connect(function (callersTree) {
-            new FunctionCallTree(JSON.parse(callersTree));
+            funcCallTree.render(JSON.parse(callersTree));
         });
 
         signalHub.funcDefTreeFuncSel.connect(function (funcId) {
-            console.log(funcId);
-            // signalHub.getFuncCallersTree(funcId);
+            signalHub.getFuncCallersTree(funcId);
         });
-
-        // Testing
-        signalHub.getFuncCallersTree(1);
     });
 }());
