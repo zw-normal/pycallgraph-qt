8bf081842e558351945d391996a414d093842a0c
Wei Zheng <Wei.Zheng@iress.com.au>
2020-11-27 05:20:00 +0000

    Call table index

--- function_call.py
+++ function_call.py
@@ -9,10 +9,12 @@ class FunctionCall(Base):
     __tablename__ = 'function_call'
 
     id = Column(Integer, primary_key=True)
-    caller_id = Column(Integer, ForeignKey('function_node.id'), nullable=False)
-    callee_id = Column(Integer, ForeignKey('function_node.id'), nullable=False)
+    caller_id = Column(
+        Integer, ForeignKey('function_node.id'), nullable=False, index=True)
+    callee_id = Column(
+        Integer, ForeignKey('function_node.id'), nullable=False, index=True)
     line_no = Column(Integer)
-    exact_call = Column(Boolean, default=False)
+    exact_call = Column(Boolean, default=False, index=True)
 
     caller = relationship(FunctionNode, foreign_keys=[caller_id])
     callee = relationship(FunctionNode, foreign_keys=[callee_id])
